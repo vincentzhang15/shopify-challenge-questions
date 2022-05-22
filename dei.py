708256023594b8e40446be1713f2df781339a456
vincentzhang15 <vincent.zhang0315@gmail.com>
2022-05-22 19:28:14 -0400

    Data Engineer Initial Upload

--- dei.py
+++ dei.py
@@ -1,16 +1,32 @@
-# Developer Intern Challenge Question
-# Requirements:
-#   Basic CRUD Functionality. You should be able to:
-#     Create inventory items
-#     Edit Them
-#     Delete Them
-#     View a list of them
+# Data Engineer Intern Challenge Question
+"""
+    Extra Information: You can, if you wish, use frameworks, libraries and external dependencies to help you get faster to the parts you are interested in building,
+                       if this helps you; or start from scratch. When building, consider how more features could be added in the future. 
 
-#   ONLY ONE OF THE FOLLOWING (We will only evaluate the first feature chosen, so please only choose one)
-#     When deleting, allow deletion comments and undeletion
-#     Ability to create warehouses/locations and assign inventory to specific locations
-#     Ability to create “shipments” and assign inventory to the shipment, and adjust inventory appropriately
-#   Authentication and CSS/Design are not required and will not be considered during evaluation.
+                       Please focus on what interests you the most. If you need inspiration, here are examples of what you can work on. IF you work on these ideas, 
+                       we recommend choosing only one or two.
+
+    Ideas:
+        SEARCH function
+            from characteristics of the images
+            from text
+            from an image (search for similar images)
+        ADD image(s) to the repository
+            one / bulk / enormous amount of images
+            private or public (permissions)
+            secure uploading and stored images
+        DELETE image(s)
+            one / bulk / selected / all images
+            Prevent a user deleting images from another user (access control)
+            secure deletion of images
+        SELL/BUY images
+            ability to manage inventory
+            set price
+            discounts
+            handle money
+"""
+
+########################
 
 import logging
 from flask import current_app, flash, Flask, Markup, redirect, render_template, make_response
@@ -18,25 +34,64 @@ from flask import request, url_for
 from flask import send_from_directory
 
 from front import front_page
+from form import form_page
 import os
 
 from app import app
-
+from dei_db import upload_image_file, get_random_images, get_image_tags
 
 @app.route('/dei')
-def dei_site():
-    return front_page("dei","")
+def dei_list():
+    images = get_random_images()
+    images = get_image_tags(images)
+    print("get_random_images:", images)
+    return front_page("dei_list",items=images)
 
-@app.route('/dei/create', methods=['GET', 'POST'])
-def dei_create():
-    return front_page("dei","")
+@app.route('/dei/add', methods=['GET', 'POST'])
+def dei_add():
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
 
-@app.route('/dei/edit', methods=['GET', 'POST'])
+        print("data:", data)
+
+        # If an image was uploaded, update the data to point to the new image.
+        upload_image_file(request.files.get('image'),data['name'],data['tags'],data['access'])
+
+    return front_page("dei_addsingle","")
+
+@app.route('/dei/addmore', methods=['GET', 'POST'])
+def dei_addmore():
+    return front_page("dei_addmore","")
+
+@app.route('/dei/uploadimages', methods=['POST'])
+def dei_upload_images():
+    data = request.form.to_dict(flat=True)
+
+    print("request.files:", request.files)
+    files =  request.files.getlist("files[]")
+    print("files:", request.files)
+    for file in files:
+        print("file:", file)
+        upload_image_file(file,data['name'],data['tags'],data['access'])
+    
+    response = make_response("OK", 200)
+    return response
+
+@app.route('/dei/image/<imageid>/<ext>', methods=['GET'])
+def dei_image(imageid,ext):
+    def read_image():
+        f = open("F:\\tempimages\\image."+imageid+"."+ext, "rb")
+        return  f.read()
+
+    response = make_response(read_image(), 200)
+    response.mimetype = "image/"+ext
+    return response
+
+
+@app.route('/dei/search', methods=['GET', 'POST'])
 def dei_edit():
-    return front_page("dei","")
+    return front_page("dei_search","")
 
 @app.route('/dei/delete', methods=['GET', 'POST'])
 def dei_delete():
     return front_page("dei","")
-
-
---
+++ dei_db.py
@@ -0,0 +1,163 @@
+import mysql.connector
+from app import dbconn
+
+def get_next_imageid():
+    db = dbconn()
+    cursor = db.cursor()
+    cursor.execute("select `id` from deitableids where `name`='deiimages'")
+    rowid = 0
+    result = cursor.fetchall()
+    for x in result:
+        rowid = x[0]+1
+        break
+    cursor.close()
+    cursor = db.cursor()
+    if rowid == 0:
+        cursor.execute("insert into deitableids (`name`, `id`) values ('deiimages', 1)")
+    else:
+        cursor.execute("update deitableids set `id`=" + str(rowid) + " where `name`='deiimages'")
+    db.commit()
+    return rowid
+
+
+def insert_image(values, tags, access):
+    db = dbconn()
+    cursor = db.cursor()
+    cursor.execute("insert into deiimages (id, name, prop, content, userid, magic) values (%s, %s, %s, %s, %s, %s)", values)
+    db.commit()
+    images = []
+    images += [{'id':values[0],'name':values[1],'tags':tags,'access':access,'content':values[3], 'userid':values[4]}]
+    return images
+
+def get_tagid(tag):
+    db = dbconn()
+    cursor = db.cursor()
+    values=[]
+    values+=[tag]
+    cursor.execute("select id from deitags where name = %s", values)
+    tagid = -1
+    result = cursor.fetchall()
+    for x in result:
+        tagid = x[0]
+        break
+    cursor.close()
+    return tagid
+
+def insert_tag(tag):
+    db = dbconn()
+    cursor = db.cursor()
+    values=[]
+    values+=[tag]
+    cursor.execute("insert into deitags (name) values (%s)", values)
+    db.commit()
+    return get_tagid(tag)
+
+def insert_image_tag(imageid, tag):
+    tagid = get_tagid(tag)
+    if tagid == -1:
+        tagid = insert_tag(tag)
+    if tagid == -1:
+        return
+    values = [imageid, tagid]
+    try:
+        db = dbconn()
+        cursor = db.cursor()
+        cursor.execute("insert into deiimagetags (imageid, tagid) values (%s, %s)", values)
+        db.commit()
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+
+def upload_image_file(img,name,tags,access):
+    """
+    Upload the user-uploaded file to Google Cloud Storage and retrieve its
+    publicly-accessible URL.
+    """
+    if not img:
+        return "No image", []
+
+    if not img.content_type.startswith('image/'):
+        return "Error file type", []
+
+    ext = img.content_type[6:]
+
+    if len(ext)<1:
+        return "Error file type", []
+
+    imageid = get_next_imageid()
+
+    imagepath = "F:\\tempimages\\"
+    filename = imagepath + "image."+str(imageid)+"."+ext
+    
+    print("..... image filename .....  ", filename)
+
+    import io
+    try:
+        with io.open(filename, "wb") as out:
+            print(" open file done")
+            image = img.read();
+            print(" read done")
+            out.write(image);
+            print(" write done")
+            out.close();
+    except IOError as err:
+        print("write file err = ", err)
+        return str(err), []
+        # raise        
+    prop = (0,1)[access == "private"]
+
+    print('Uploaded file:', img.filename, " contentType:", img.content_type, " id:", imageid, "  name:",name, " tags:", tags, "  access:", access, "prop:", prop)
+    #values = []
+    #values += [imageid]
+    #values += [name]
+    #values += [prop]
+    #values += [img.content_type]
+    userid = 'default'
+    magic = '.'
+    images = insert_image([imageid, name, prop, img.content_type, userid, magic], tags, access)
+    for rawtag in tags.split(','):
+        tag = rawtag.strip()
+        if len(tag)>0:
+            insert_image_tag(imageid, tag)
+    return "", images
+
+def get_random_images():
+    db = dbconn()
+    cursor = db.cursor()
+    cursor.execute("select id, name, prop, content,userid, last_updated from deiimages order by RAND() limit 10")
+    result = cursor.fetchall()
+    images = {}
+    for x in result:
+        images[x[0]] = {'id':x[0],'name':x[1],'prop':x[2],'content':x[3], 'userid':x[4], 'updated':x[5]}
+    cursor.close()
+    return images
+
+def get_image_tags(images):
+    print("get_image_tags for x in images) images = ", images)
+    sql = "select imageid,tagid,name from deiimagetags left join deitags on tagid=id where imageid in "
+    i = 0
+    values = []
+    for x in images:
+        sql += ('(',',')[i!=0] + '%s'
+        i = i + 1
+        print("get_image_tags for x in images) x = ", x)
+        values += [x]
+    sql += ')'
+    print("get_image_tags: sql=", sql)    
+
+    db = dbconn()
+    cursor = db.cursor()
+    cursor.execute(sql, values)
+    result = cursor.fetchall()
+    tags = {}
+    for x in result:
+        if x[0] not in tags.keys():
+            tags[x[0]] = x[2]
+        else:
+            tags[x[0]] += ',' + x[2]
+    cursor.close()
+    print("get_image_tags: tags=", tags)
+
+    for x in images:
+        images[x]['tags'] = tags[x]
+    print("get_image_tags: images=", images)
+    return images
